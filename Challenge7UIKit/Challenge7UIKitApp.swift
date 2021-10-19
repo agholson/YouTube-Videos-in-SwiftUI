932a7f637235e9ac7002b712cc7ee15acddf2895
agholson <87455899+agholson@users.noreply.github.com>
2021-10-19 17:11:55 -0400

    Initial Commit

---
+++ Challenge7UIKit/Challenge7UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge7UIKitApp.swift
+//  Challenge7UIKit
+//
+//  Created by Shepherd on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge7UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
